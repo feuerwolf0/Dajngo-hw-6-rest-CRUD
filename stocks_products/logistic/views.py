7701f993b58caeca0c8ff3bf0e29b8ed2c0ec338
Poletaev Kirill <poletaev777@gmail.com>
2023-08-29 03:11:18 +0300

    initial commit

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,24 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+
+from .filters import StockFilter
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['id', 'title', 'description']
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = StockFilter
+    filterset_fields = ['positions__product__id']
+    search_fields = ['positions__product__title', 'positions__product__description']
